7c1cabba7ba7fff17c59279781b469dc02a06c1c
pucheng <pucheng@xiaomalixing.com>
2018-10-15 19:18:05 +0800

    LocalASR demo

---
+++ sample/src/main/java/com/aispeech/sample/AppKey.java
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ * Copyright 2014 AISpeech
+ ******************************************************************************/
+package com.aispeech.sample;
+
+//TODO 修改完毕后去除
+public final class AppKey {
+
+    public static final String APPKEY = "1498016899859681";// 测试激活码用的appkey
+    public static final String SECRETKEY = "bb82353a69a5e406c91b5b56223e15e6";//添加您的SECRETKEY"
+    
+    
+}
